e790839dd507d03c8a68b5d9ef5676b5baee311e
Harsh Vardhan Ladha <harsh.l@directi.com>
2018-05-25 18:16:33 +0530

    Added argumented Task feature

--- timertask.go
+++ timertask.go
@@ -3,30 +3,55 @@ package timertask
 import "time"
 
 type TimerTask struct {
-	function func(interface{})
+	task Task
 	duration time.Duration
 }
 
-func newTimerTask(f func(interface{}), d time.Duration) *TimerTask {
+type Task struct {
+	run func(interface{})
+	data interface{}
+}
+
+// Creates a new Task with argument which needs to be passed to a function to be scheduled
+func NewTaskWithArgument(f func(interface{}), arg interface{}) (*Task) {
+	return &Task{
+		run: f,
+		data: arg,
+	}
+}
+
+// Creates a new Task without argument which needs to be passed to a function to be scheduled
+func NewTask(f func(interface{})) (*Task) {
+	run := func(interface{}) {
+		f()
+	}
+
+	return &Task{
+		run: run,
+	}
+}
+
+func newTimerTask(t Task, d time.Duration) *TimerTask {
 	return &TimerTask{
-		function: f,
+		task: t,
 		duration: d,
 	}
 }
 
 // Schedules a function `f` to run at a `d` Duration
-func Schedule(f func(interface{}), d time.Duration) {
-	timerTask := newTimerTask(f, d)
+func Schedule(t Task, d time.Duration) {
+	timerTask := newTimerTask(t, d)
 	ticker := time.NewTicker(timerTask.duration)
-	taskInvoker(ticker, f)
+	taskInvoker(ticker, t)
 }
 
-func taskInvoker(ticker *time.Ticker, f func(interface{})) {
+
+func taskInvoker(ticker *time.Ticker, t Task) {
 	go func() {
 		for {
 			select {
 			case <-ticker.C:
-				f()
+				t.run(t.data)
 			}
 		}
 	}()
