ae545f27b492699b1856040278901556d62cb3f8
Harsh Vardhan Ladha <harsh.l@directi.com>
2018-05-26 12:40:52 +0530

    Added example for timertask without args in function

---
+++ examples/example_01.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"github.com/harshvladha/timertask"
+	"time"
+)
+
+func main() {
+	task := timertask.NewTask(func() {
+		fmt.Println("Saying Hello, Go?")
+	})
+	scheduler := timertask.Schedule(*task, 1*time.Second)
+	for i := 0; i < 10; i++ {
+		time.Sleep(1 * time.Second)
+	}
+	fmt.Println("Killing timertask")
+	scheduler.Stop()
+	time.Sleep(5 * time.Second)
+}
